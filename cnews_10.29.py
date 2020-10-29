054191df3a73a2cbf7a08206280d8b292c0de47d
Zhang Yuxi <zhangyuxi@ZhangdeMacBook-Pro.local>
2020-10-29 22:37:11 +0800

    second commit

--- cnews_10.29.py
+++ cnews_10.29.py
@@ -10,7 +10,7 @@ import torch.utils.data as Data
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 #设置数据目录
 vocab_file = 'cnews.vocab.txt'
-train_file = 'cnews.train.txt'
+train_file = 'cnews.train1.txt'
 test_file = 'cnews.test.txt'
 val_file = 'cnews.val.txt'
 # 获取文本的类别及其对应id的字典
@@ -26,7 +26,7 @@ vocab_size = len(words)
 
 # 数据加载及分批
 # 获取训练数据每个字的id和对应标签的one-hot形式
-x_train, y_train = process_file('cnews.train.txt', word_to_id, cat_to_id, 600)
+x_train, y_train = process_file('cnews.train1.txt', word_to_id, cat_to_id, 600)
 #print('x_train=', x_train)
 x_val, y_val = process_file('cnews.val.txt', word_to_id, cat_to_id, 600)
 
@@ -46,11 +46,13 @@ def train():
     #定义损失函数
     Loss = nn.MultiLabelSoftMarginLoss()
     optimizer = optim.Adam(model.parameters(), lr = 0.001)
+
 #保存最好模型，先给一个定义为0
     best_val_acc = 0
     for epoch in range(10):
-       # print('epoch=',epoch)
+        # print('epoch=',epoch)
         #分批训练
+        accuracy_array0= np.array([])
         for step, (x_batch, y_batch) in enumerate(train_loader):
             x = x_batch.to(device)
             y = y_batch.to(device)
@@ -63,71 +65,26 @@ def train():
             loss.backward()
             optimizer.step()
             accuracy0 = np.mean((torch.argmax(out, 1)==torch.argmax(y,1)).cpu().numpy())
-            accuracy_array0= np.array([])
             accuracy_array0 = np.append(accuracy_array0, accuracy0)
-            accuracy_train = np.mean(accuracy_array0)
-            print('accuracy_train:',accuracy_train)
-        
+        accuracy_train = np.mean(accuracy_array0)
+        print('accuracy_train:',accuracy_train)       
 #对模型进行验证
-        if  (epoch+1)% 5 == 0:
+        if  (epoch+1)% 5 == 0:  
             for step, (x_batch,y_batch) in enumerate(val_loader):
                 x = x_batch.to(device)
                 y = y_batch.to(device)
                 out = model(x)
                 #计算准确率
-                accuracy1 = np.mean((torch.argmax(out, 1)==torch.argmax(y,1)).cpu().numpy())
+        accuracy1 = np.mean((torch.argmax(out, 1)==torch.argmax(y,1)).cpu().numpy())    
+        accuracy_array1= np.array([])
         if accuracy1 > best_val_acc:
             torch.save(model,'model.pkl')
             best_val_acc = accuracy1
             print('model.pkl saved')
-            accuracy_array1= np.array([])
-            accuracy_array1 = np.append(accuracy_array1, accuracy1)
-            accuracy_test = np.mean(accuracy_array1)
-            print('accuracy_test:',accuracy_test)
-            #print('best_accuracy:',best_val_acc)     
-
+            accuracy_array1 = np.append(accuracy_array1, best_val_acc)
+        accuracy_test = np.mean(accuracy_array1)
+        print('accuracy_test:',accuracy_test)
+    #print('best_accuracy:',best_val_acc)     
 if __name__ == '__main__':
     train()
 
-"""ccuracy: 0.34375
-accuracy: 0.375
-accuracy: 0.265625
-accuracy: 0.4375
-accuracy: 0.3359375
-accuracy: 0.3984375
-accuracy: 0.4140625
-accuracy: 0.3359375
-accuracy: 0.3359375
-accuracy: 0.390625
-accuracy: 0.3828125
-accuracy: 0.34375
-accuracy: 0.3671875
-accuracy: 0.328125
-accuracy: 0.265625
-accuracy: 0.4453125
-accuracy: 0.4140625
-accuracy: 0.3125
-accuracy: 0.296875
-accuracy: 0.3125
-accuracy: 0.3515625
-accuracy: 0.328125
-accuracy: 0.4296875
-accuracy: 0.3828125
-accuracy: 0.328125
-accuracy: 0.34375
-accuracy: 0.390625
-accuracy: 0.4453125
-accuracy: 0.3203125
-accuracy: 0.390625
-accuracy: 0.34375
-accuracy: 0.3359375
-accuracy: 0.328125
-accuracy: 0.3046875
-accuracy: 0.3828125
-accuracy: 0.3671875
-accuracy: 0.3203125
-accuracy: 0.375
-accuracy: 0.3515625
-accuracy: 0.3046875
-accuracy: 0.359375
-accuracy: 0.34375"""
